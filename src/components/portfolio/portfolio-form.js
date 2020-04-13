8d47b02f5c336fcccce1e787d19d5851db4c0c91
r0ckst23dy <r0ck1ngst34dy@outlook.com>
2020-04-13 12:44:58 -0400

    added forms, grid, and portfolio-form scss files for styles in portfolio-form.js

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -130,86 +130,84 @@ export default class PortfolioForm extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>PortfolioForm</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <input
-              type="text"
-              name="name"
-              placeholder="Portfolio Item Name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              name="url"
-              placeholder="URL"
-              value={this.state.url}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="position"
-              placeholder="position"
-              value={this.state.position}
-              onChange={this.handleChange}
-            />
-            <select
-              name="category"
-              value={this.state.category}
-              onChange={this.handleChange}
-            >
-              <option value="eCommerce">eCommerce</option>
-              <option value="Scheduling">Shceduling</option>
-              <option value="Enterprise">Enterprise</option>
+      <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
+        <div className="two-column">
+          <input
+            type="text"
+            name="name"
+            placeholder="Portfolio Item Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="url"
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="two-column">
+          <input
+            type="text"
+            name="position"
+            placeholder="position"
+            value={this.state.position}
+            onChange={this.handleChange}
+          />
+          <select
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+            className="select-element"
+          >
+            <option value="eCommerce">eCommerce</option>
+            <option value="Scheduling">Shceduling</option>
+            <option value="Enterprise">Enterprise</option>
 
-            </select>
-          </div>
-          <div>
-            <textarea
-              type="text"
-              name="description"
-              placeholder="description"
-              value={this.state.description}
-              onChange={this.handleChange}
-            />
-          </div>
+          </select>
+        </div>
+        <div className="one-column">
+          <textarea
+            type="text"
+            name="description"
+            placeholder="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
 
-          <div className="image-uploader">
-            <DropzoneComponent
-              ref={this.thumbRef}
-              config={this.componentConfig()}
-              djsConfig={this.djsConfig()}
-              eventHandlers={this.handleThumbDrop()}
-
-
-            />
-            <DropzoneComponent
-              ref={this.bannerRef}
-              config={this.componentConfig()}
-              djsConfig={this.djsConfig()}
-              eventHandlers={this.handleBannerDrop()}
-
-            />
-            <DropzoneComponent
-              ref={this.logoRef}
-              config={this.componentConfig()}
-              djsConfig={this.djsConfig()}
-              eventHandlers={this.handleLogoDrop()}
-
-            />
+        <div className="image-uploader three-column">
+          <DropzoneComponent
+            ref={this.thumbRef}
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleThumbDrop()}
 
 
-          </div>
+          />
+          <DropzoneComponent
+            ref={this.bannerRef}
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleBannerDrop()}
 
-          <div>
-            <button type="submit">Save</button>
-          </div>
-        </form>
-      </div>
+          />
+          <DropzoneComponent
+            ref={this.logoRef}
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleLogoDrop()}
+
+          />
+
+
+        </div>
+
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
     );
   }
 }
