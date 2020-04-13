87d386bd6824ef7b93c46e247bbc75b2dbb671dc
r0ckst23dy <r0ck1ngst34dy@outlook.com>
2020-04-13 13:16:58 -0400

    updated save button styles and added child component for dropzone component

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -183,29 +183,33 @@ export default class PortfolioForm extends Component {
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleThumbDrop()}
+          >
+            <div className="dz-message">Thumbnail</div>
+          </DropzoneComponent>
 
-
-          />
           <DropzoneComponent
             ref={this.bannerRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleBannerDrop()}
+          >
+            <div className="dz-message">Banner</div>
+          </DropzoneComponent>
 
-          />
           <DropzoneComponent
             ref={this.logoRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleLogoDrop()}
-
-          />
+          >
+            <div className="dz-message">Logo</div>
+          </DropzoneComponent>
 
 
         </div>
 
         <div>
-          <button type="submit">Save</button>
+          <button className="save-btn" type="submit">Save</button>
         </div>
       </form>
     );
