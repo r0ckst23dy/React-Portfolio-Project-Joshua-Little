82ddfc5a2d81cb6c27c99f863bf0fcc4be11ac1e
r0ckst23dy <r0ck1ngst34dy@outlook.com>
2020-04-29 07:54:55 -0400

    added getBase64 method and upfile method to  rech-text-editor file

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -11,6 +11,8 @@ export default class RichTextEditor extends Component {
         this.state = { editorState: EditorState.createEmpty() };
 
         this.onEditorStateChange = this.onEditorStateChange.bind(this);
+        this.getBase64 = this.getBase64.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
     }
 
     // method that handles changes from initial state in rich text editor 
@@ -29,6 +31,20 @@ export default class RichTextEditor extends Component {
         );
     }
 
+    getBase64(file, callback) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => callback(reader.result);
+        reader.onerror = error => { };
+    }
+
+    //method to handle the process of uploading 
+    uploadFile(file) {
+        return new Promise((resolve, reject) => {
+            this.getBase64(file, data => resolve({ data: { link: data } }));
+        });
+    }
+
     render() {
         return (
             <div>
@@ -37,6 +53,19 @@ export default class RichTextEditor extends Component {
                     wrapperClassName="demo-wrapper"
                     editorClassName="demo-editor"
                     onEditorStateChange={this.onEditorStateChange}
+                    toolbar={{
+                        inline: { inDropdown: true },
+                        list: { inDropdown: true },
+                        textAlign: { inDropdown: true },
+                        links: { inDropdown: true },
+                        history: { inDropdown: true },
+                        image: {
+                            uploadCallback: this.uploadFile,
+                            alt: { present: true, mandatory: false },
+                            previewImage: true,
+                            inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg "
+                        }
+                    }}
                 />
             </div>
         );
