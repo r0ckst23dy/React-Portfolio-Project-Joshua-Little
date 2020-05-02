20b0a3dd2a431d764ef4723893a28fb2d76273a7
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-02 09:08:04 -0400

    added full filtering feature

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -23,23 +23,32 @@ export default class PortfolioContainer extends Component {
 
     //method for filtering api data 
     handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        });
-
+        if ( filter === "CLEAR_FILTERS") {
+            this.getPortfolioItems();
+        }else
+        {
+            this.getPortfolioItems(filter);
+        }
     }
 
     //method for get request
-    getPortfolioItems() {
+    getPortfolioItems(filter = null) {
         axios
             .get('https://jtlittle.devcamp.space/portfolio/portfolio_items')
             .then(response => {
+                if (filter) {
+                    this.setState({
+                        data: response.data.portfolio_items.filter(item => {
+                            return item.category === filter;
+                        })
+                    });
+                } else {
+                    this.setState({
+                        data: response.data.portfolio_items
+                    });
+                }
                 // handle success
-                this.setState({
-                    data: response.data.portfolio_items
-                })
+
             })
             .catch(error => {
                 // handle error
@@ -64,11 +73,17 @@ export default class PortfolioContainer extends Component {
         }
 
         return (
-            <div className="portfolio-items-wrapper">
-                <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button className="btn" onClick={() => this.handleFilter('Home-Fragrance')}>Home-Fragrance</button>
-                <button className="btn" onClick={() => this.handleFilter('Lodge')}>Lodge</button>
-                {this.portfolioItems()}
+            <div className="homepage-wrapper">
+                <div className="filter-links">
+                    <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                    <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                    <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>                     
+                    <button className="btn" onClick={() => this.handleFilter('CLEAR_FILTERS')}>All</button>                     
+                </div>
+                <div className="portfolio-items-wrapper">
+                        {this.portfolioItems()}
+                </div>
+                
             </div>
 
         );
